8587bd7e4088633b9b3dc439626f46c684e39c41
Rishav Anand <rishavdipsite@gmail.com>
2019-11-20 23:11:38 +0530

    open resume in new tab

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
 particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
-
-$('#resume-download-btn').click(() => {
-  window.location = './documents/rishav_resume.pdf';
-});
