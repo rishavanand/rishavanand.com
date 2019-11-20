0602e45fab18c799f1b4277508319eb69e501506
Rishav Anand <rishavdipsite@gmail.com>
2019-11-20 23:02:33 +0530

    added details and resume

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+$('#resume-download-btn').click(() => {
+  window.location = './documents/rishav_resume.pdf';
+});
